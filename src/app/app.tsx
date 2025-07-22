272b5d4b784f411d456eb221c68483ca0918f518
Andydevs <akanshul97@gmail.com>
2025-07-22 00:48:49 -0400

    Implement format number and tested multiple electron observable

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,16 +2,17 @@
 import { useObservableNext } from './rxhooks';
 import { electronCount$, emitElectronSubject$ } from './rxstate';
 
+const numFormatter = new Intl.NumberFormat("en-us", { notation: "compact" })
+function Number({ value }: { value: number }) {
+    const numFormat = numFormatter.format(value)
+    return <span>{numFormat}</span>
+}
 
 export function App() {
     const electronCount = useObservableNext(electronCount$)
 
-    const handleClick = () => {
-        emitElectronSubject$.next(1)
-    }
-
     return (<div>
-        <p>Electrons: {electronCount}</p>
-        <p><button onClick={handleClick}>Electron</button></p>
+        <p>Electrons: <Number value={electronCount}/></p>
+        <p><button onClick={() => emitElectronSubject$.next(1)}>Electron</button></p>
     </div>)
 }
